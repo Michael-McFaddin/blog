a7ebfcedc9a5bdc4333f621ddd3eb65298fa3038
mike <mikemcfaddin@gmail.com>
2021-01-31 20:51:07 -0500

    more work on redux

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,40 @@
-import jsonPlaceHolder from '../apis/jsonPlaceHolder';
+import _ from 'lodash';
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPostsAndUsers = () => async dispatch => {
+  console.log('About to fetch posts');
+  await dispatch(fetchPosts());
+  console.log('Fetched posts!');
+};
 
 export const fetchPosts = () => async dispatch => {
-    const response = await jsonPlaceHolder.get('/posts');
+  const response = await jsonPlaceholder.get('/posts');
 
-    dispatch({ type: 'FETCH_POST', payload: response })
+  dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
+
+export const fetchUser = id => async dispatch => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: 'FETCH_USER', payload: response.data });
+};
+
+// export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+//   const _fetchUser = _.memoize(async (id, dispatch) => {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({ type: 'FETCH_USER', payload: response.data });
+// });
+
+//above is refactor
+// export const fetchUser = id => dispatch => {
+//   _fetchUser(id, dispatch);
+// };
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({ type: 'FETCH_USER', payload: response.data });
+// });
+
+
